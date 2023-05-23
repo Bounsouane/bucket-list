ef245feb2dc976a433177ea5ade886b571d5da1e
Bounsouane <n.nanteau@gmail.com>
2023-05-23 13:44:52 +0200

    Mise à jour Base de données + Affichage Liste

---
+++ src/Form/WishFormType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Wish;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class WishFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('description', TextareaType::class, [
+                //désactive la vérif html uniquement pour ca
+                'required'=> false,
+                'label'=> 'Describe your wish : '
+            ])
+            ->add('author', TextType::class)
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Wish::class,
+            //désactive la vérif html pour tous les champs
+            'required'=>false
+        ]);
+    }
+}
